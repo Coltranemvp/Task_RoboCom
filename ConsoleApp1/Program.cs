44046e0566d59870fa93ea0f9b7b812c1cc43409
Alexander Subbptin <coltranexpro@yandex.ru>
2021-06-20 20:05:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Linq;
+using Task_RoboCom;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ввод: ");
+            var files = Directory.GetFiles($"{Console.ReadLine()}", "*.*", SearchOption.AllDirectories)
+           .Where(s => s.EndsWith(".dll"));
+            List<AddFile> rooud = new List<AddFile>();
+            foreach (var t in files)
+            {
+                rooud.Add(new AddFile(t));
+            }
+            string path;
+            Assembly SampleAssembly;
+            foreach (AddFile t in rooud)
+            {
+                path = t.Name;
+                SampleAssembly = Assembly.LoadFrom(path);
+                foreach (var ts in SampleAssembly.GetTypes())
+                {
+                    Console.WriteLine(ts.Name + "  :");
+                    foreach (var qq in ts.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly| BindingFlags.Public))
+                    {
+                         Console.WriteLine("Метод :"+qq.Name); 
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+               
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_RoboCom/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
+
+
+namespace Task_RoboCom
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> rooud = new List<string>();
+            var files = Directory.GetFiles($"{box.Text}", "*.*", SearchOption.AllDirectories)
+           .Where(s => s.EndsWith(".dll"));
+            foreach (string t in files)
+            {
+                rooud.Add(t);
+            }
+            box1.Clear();
+            foreach (string t in rooud)
+            {
+                box1.Text += t+ Environment.NewLine;
+                foreach (Type type in GetTypes(Assembly.LoadFile(t))) 
+                {
+                    box1.Text += type.Name+ Environment.NewLine;
+                    foreach (MemberInfo mi in type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Public))
+                    {
+                        box1.Text += $"{mi.MemberType} {mi.Name}"+ Environment.NewLine;
+                    }
+                }
+                box1.Text += Environment.NewLine+Environment.NewLine;
+            }
+        }
+
+
+        public IEnumerable<Type> GetTypes( Assembly assembly)
+        {
+            try
+            {
+                return assembly.GetTypes();
+            }
+            catch (ReflectionTypeLoadException ex)
+            {
+                return ex.Types.Where(t => t != null);
+            }
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+
+}
